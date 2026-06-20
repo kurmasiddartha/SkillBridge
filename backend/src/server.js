0b2b5ae7bf8f11399e7a9adbc379316d12bbcc87
K Siddartha <kurmasiddartha0401@gmail.com>
2026-06-20 17:22:38 +0530

    updated feature

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,5 +13,5 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`SkillBridge Server running on port ${PORT}`);
 });
